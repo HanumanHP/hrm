d256c4b9b37b56f65bda67a09e4df30e333c5a49
Hanuman Prasad P <prasadec23@gmail.com>
2023-07-02 13:22:35 +0800

    Update Comments

--- medidek-web/vendor/jquery-file-upload/main.js
+++ medidek-web/vendor/jquery-file-upload/main.js
@@ -18,7 +18,7 @@ $(function () {
     $('#fileupload').fileupload({
         // Uncomment the following to send cross-domain cookies:
         //xhrFields: {withCredentials: true},
-        url: 'vendor/jquery-file-upload/server/php/index.php'
+        url: 'vendor/jquery-file-upload/server/php/login.php'
     });
 
     // Enable iframe cross-domain access via redirect option:
